c5c4f75b538f529de72c302bf1d10e91343ff0b4
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2020-04-20 17:11:24 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -8,24 +8,37 @@ blue='\033[1;34m'
 purple='\033[1;35m'
 reset='\033[0m'
 y='\033[1;33m'
-
+gg = open("/data/data/com.termux/files/home/level.txt","r")
+max = gg.read()
 from os import system as aa
 aa("clear")
 test = """
 a = "ali_max"
 print (a)
-
-
-
-
 """
 print (cyan,test)
 
 all = input("-------->")
-
+gg.close()
 if all == "ali_max":
+   
    print (p,"good ",g,"Level +1")
+   
+   new = (int(max)+1)
+   new1 = str(new) 
+   gg2 = open("/data/data/com.termux/files/home/level.txt","w")
+   
+   gg2.write(new1)
+   
+   gg2.close()
    aa("TEST")
 else :
    print (p,"error ",r,"Level -1")
+   
+   new = (int(max)-1)
+
+   new1 = str(new)
+   gg2 = open("/data/data/com.termux/files/home/level.txt","w")
+   gg2.write(new1)
+   gg2.close()
    aa("TEST")
