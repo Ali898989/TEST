b5d462c57ded6ed45461de922b2302861c7af5c4
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2020-04-20 17:57:18 +0200

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,6 +1,6 @@
 cd
 cd TEST
-cp t.sh /data/data/com.termux/files/usr/bin/TEST1
+cp t.sh /data/data/com.termux/files/usr/bin/TEST
 chmod 777 /data/data/com.termux/files/usr/bin/TEST
 cd 
 echo "1" >> level.txt
